ea6c4fe9965770e7116ea2eca664aa9a0a0c446e
Silvio O. <socarrandin@gmail.com>
2025-08-16 16:33:20 -0400

    feat: image

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   // Optimización de imágenes
   images: {
     formats: ["image/webp", "image/avif"],
-    deviceSizes: [140, 640, 750, 828, 1080, 1200],
+    deviceSizes: [140, 200, 640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 
